a536aa853e10bf28390e9027a0c788f6668759e5
Rhendi <mudauml15@hotmail.com>
2024-06-24 21:50:37 +0200

    side bar ui

--- app/(root)/(home)/layout.tsx
+++ app/(root)/(home)/layout.tsx
@@ -1,11 +1,14 @@
 import React, { Children, ReactNode } from "react";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
 
 const HomeLayout = ({ children }: { children: ReactNode }) => {
   return (
     <main className="relative">
-      Navbar
+      <Navbar/>
       <div className="flex">
-        Sidebar
+        <Sidebar />
         <section
           className="flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md: pd-14
             sm:px-14">
