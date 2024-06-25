8f3c9c8df76c023c21f307022e44db79edd12700
Rhendi <mudauml15@hotmail.com>
2024-06-25 11:59:18 +0200

    sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,19 +1,42 @@
-import React from 'react'
+"use client";
+import React from "react";
+import { sidebarLinks } from "@/constraints";
+import { usePathname } from "next/navigation";
+import { cn } from "@/lib/utils";
+import Link from "next/link";
 
 const Sidebar = () => {
+  const pathname = usePathname();
   return (
-    <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between
-    bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
+    <section
+      className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between
+    bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]"
+    >
+      <div className="flex flex- flex-col gap-6">
+        {sidebarLinks.map((link) => {
+          const isActive = pathname === link.route || pathname.startsWith(link.route);
 
-        <div className='flex flex- flex-col gap-6'>
+          return (
+            <Link
+            
+            href={link.route}
+            key={link.label}
+            className={cn(
+              "flex gap-4 items-center p-4 rounded-lg justify-start",
+              { "bg-blue1": isActive }
+            )}
+          >
+            {link.label}
 
-        </div>
-        
-        Sidebar
-        
-        
-        </section>
-  )
-}
+            </Link>
 
-export default Sidebar
+
+          );
+        })}
+      </div>
+      Sidebar
+    </section>
+  );
+};
+
+export default Sidebar;
---
+++ constraints/index.ts
@@ -0,0 +1,35 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/Home.svg'
+    },
+
+    {
+        label: 'Upcoming',
+        route: '/Upcoming',
+        imgUrl: '/icons/upcoming.svg',
+    },
+
+
+    {
+        label: 'Previous',
+        route: '/Previous',
+        imgUrl: '/icons/Previous.svg',
+    },
+
+    {
+        label: 'Recordings',
+        route: '/Recordings',
+        imgUrl: '/icons/Video.svg',
+    },
+
+    {
+        label: 'Personal Room',
+        route: '/Personal-room',
+        imgUrl: '/icons/add-personal.svg',
+    },
+
+
+
+];
